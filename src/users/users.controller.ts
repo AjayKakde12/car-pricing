8f4f4df47e13a72e1e08bc3ed16fd5195ced7a1a
Ajay Kakde <kakdeajay12@yahoo.com>
2026-01-24 11:06:36 +0530

    new/features User Register, Get, Post, Delete, Update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
-    constructor(private userService: UsersService) {}
+    constructor(private readonly userService: UsersService) {}
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
+    public createUser(@Body() body: CreateUserDto) {
         this.userService.create(body.email, body.password);
     }
+
+    @Get('/user/:id')
+    public async findUser(@Param('id') id: string) {
+        return await this.userService.findOne(parseInt(id));
+    }
+
+    @Get('/users')
+    public async findAllUsers(@Query('email') email: string) {
+        return await this.userService.find(email);
+    }
+
+    @Delete('/user/:id')
+    public async removeUser(@Param('id') id: string) {
+        return await this.userService.remove(parseInt(id));
+    }
+
+    @Patch('/user/:id')
+    public async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return await this.userService.update(parseInt(id), body)
+    }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>) {}
-    create(email: string, password: string) {
+    constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
+    public create(email: string, password: string) {
         const user = this.repo.create({ email, password });
         return this.repo.save(user);
     }
+
+    public async findOne(id: number) {
+        const user = await this.repo.findOneBy({id});
+        if(!user) {
+            throw new NotFoundException('User not found');
+        }
+        return user;
+    }
+
+    public async find(email: string) {
+        return await this.repo.find({ where: { email } })
+    }
+
+    public async update(id: number, attrs: Partial<User>) {
+        let user = await this.findOne(id);
+        if(!user) {
+            throw new NotFoundException('User not found');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    public async remove(id: number) {
+        const user = await this.findOne(id);
+        if(!user) {
+            throw new NotFoundException('User not found');
+        }
+        return this.repo.remove(user);
+    }
+
 }
